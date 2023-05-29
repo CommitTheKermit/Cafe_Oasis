8b6d9ad7c601e568a6d601e7447a00cb27ed3de1
aseong.yu <1813ee@gmail.com>
2023-05-29 16:42:34 +0900

    이메일 인증 추가

--- ex1/app/src/main/java/com/example/ex1/Activities/EmailAuthenticationActivity.java
+++ ex1/app/src/main/java/com/example/ex1/Activities/EmailAuthenticationActivity.java
@@ -9,35 +9,37 @@ import android.widget.TextView;
 
 import com.example.ex1.R;
 
-public class Join3 extends AppCompatActivity {
+public class EmailAuthenticationActivity extends AppCompatActivity {
 
-    TextView btn_next_sign3;
-    View arrow3;
+    TextView btn_check_email;
+    View email_auth_arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join3);
+        setContentView(R.layout.activity_email_authentication);
 
-        arrow3 = findViewById(R.id.arrow3);
-        btn_next_sign3 = findViewById(R.id.btn_next_sign3);
+        btn_check_email = findViewById(R.id.btn_check_email);
+        email_auth_arrow = findViewById(R.id.email_auth_arrow);
 
-        btn_next_sign3.setOnClickListener(new View.OnClickListener() {
+        btn_check_email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Join3.this,LoginActivity.class);
+                Intent intent = new Intent(EmailAuthenticationActivity.this, Join2.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        arrow3.setOnClickListener(new View.OnClickListener() {
+
+        email_auth_arrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Join3.this,Join2.class);
+                Intent intent = new Intent(EmailAuthenticationActivity.this, Join1.class);
                 startActivity(intent);
                 finish();
             }
         });
+
     }
 }

058a6bdf48dea2c03b9a50d0c79e971b6f544015
aseong.yu <1813ee@gmail.com>
2023-06-05 13:16:01 +0900

    키워드 점수 재수정

--- FrontEnd/ex1/app/src/main/java/com/example/ex1/Activities/Keyword.java
+++ FrontEnd/ex1/app/src/main/java/com/example/ex1/Activities/Keyword.java
@@ -14,11 +14,11 @@ import com.google.android.material.slider.RangeSlider;
 
 public class Keyword extends AppCompatActivity {
 
-    int[] keyword_point_arr = {1,1,1,0,1,1,1,1,1,1,0};
+    int[] keyword_point_arr = {1,1,1,1,0,1,1,3,1,1,0,0};
 
-    RangeSlider rangeSlider_dessert,rangeSlider_various,rangeSlider_special,rangeSlider_size,
-            rangeSlider_Landscape,rangeSlider_concentration,rangeSlider_trendy,rangeSlider_unique,rangeSlider_price;
-    CheckBox check_parking, check_gift;
+    RangeSlider rangeSlider_beverage, rangeSlider_dessert,rangeSlider_various,rangeSlider_special,rangeSlider_size,
+            rangeSlider_Landscape,rangeSlider_concentration,rangeSlider_trendy,rangeSlider_price;
+    CheckBox check_parking, check_price,check_gift;
 
     TextView btn_next_keyword;
     @Override
@@ -26,6 +26,7 @@ public class Keyword extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyword);
 
+        rangeSlider_beverage = findViewById(R.id.rangeSlider_beverage);
         rangeSlider_dessert = findViewById(R.id.rangeSlider_dessert);
         rangeSlider_various = findViewById(R.id.rangeSlider_various);
         rangeSlider_special = findViewById(R.id.rangeSlider_special);
@@ -33,10 +34,10 @@ public class Keyword extends AppCompatActivity {
         rangeSlider_Landscape = findViewById(R.id.rangeSlider_Landscape);
         rangeSlider_concentration = findViewById(R.id.rangeSlider_concentration);
         rangeSlider_trendy = findViewById(R.id.rangeSlider_trendy);
-        rangeSlider_unique = findViewById(R.id.rangeSlider_unique);
-        rangeSlider_price = findViewById(R.id.rangeSlider_price);
+
         check_parking = findViewById(R.id.check_parking);
         check_gift = findViewById(R.id.check_gift);
+        check_price = findViewById(R.id.check_price);
         btn_next_keyword = findViewById(R.id.btn_next_keyword);
 
         rangeSlider_dessert.addOnSliderTouchListener(rangeSliderTouchListener_dessert);
@@ -46,33 +47,34 @@ public class Keyword extends AppCompatActivity {
         rangeSlider_Landscape.addOnSliderTouchListener(rangeSliderTouchListener_Landscape);
         rangeSlider_concentration.addOnSliderTouchListener(rangeSliderTouchListener_concentration);
         rangeSlider_trendy.addOnSliderTouchListener(rangeSliderTouchListener_trendy);
-        rangeSlider_unique.addOnSliderTouchListener(rangeSliderTouchListener_unique);
-        rangeSlider_price.addOnSliderTouchListener(rangeSliderTouchListener_price);
+        rangeSlider_beverage.addOnSliderTouchListener(rangeSliderTouchListener_beverage);
+
 
         btn_next_keyword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(keyword_point_arr[0]+keyword_point_arr[1]+keyword_point_arr[2]<=6&&keyword_point_arr[4]+keyword_point_arr[5]<=4
-                &&keyword_point_arr[7]+keyword_point_arr[8]<=4){
+                if(keyword_point_arr[0]+keyword_point_arr[1]+keyword_point_arr[2]+keyword_point_arr[3]<=7
+                        &&keyword_point_arr[5]+keyword_point_arr[6]<=4){
 
                     if(check_parking.isChecked()){
-                        keyword_point_arr[3]=1;
+                        keyword_point_arr[4]=1;
+                    }
+                    if(check_price.isChecked()){
+                        keyword_point_arr[10]=1;
                     }
                     if(check_gift.isChecked()){
-                        keyword_point_arr[10]=1;
+                        keyword_point_arr[11]=1;
                     }
 
                 }
-                else if(keyword_point_arr[0]+keyword_point_arr[1]+keyword_point_arr[2]>6){
+                else if(keyword_point_arr[0]+keyword_point_arr[1]+keyword_point_arr[2]>7){
                     Toast.makeText(getApplicationContext(),"Desert, Various_menu, Special_menu의 총합이 6 이하여야 합니다.",Toast.LENGTH_SHORT).show();
                 }
-                else if(keyword_point_arr[4]+keyword_point_arr[5]>4){
+                else if(keyword_point_arr[5]+keyword_point_arr[6]>4){
                     Toast.makeText(getApplicationContext(),"Large_store, Landscape의 총합이 4 이하여야 합니다.",Toast.LENGTH_SHORT).show();
                 }
-                else if(keyword_point_arr[7]+keyword_point_arr[8]>4){
-                    Toast.makeText(getApplicationContext(),"Trendy_store, Unique_store의 총합이 4 이하여야 합니다.",Toast.LENGTH_SHORT).show();
-                }
+
 
             }
         });
@@ -80,7 +82,7 @@ public class Keyword extends AppCompatActivity {
 
     }
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_dessert =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_beverage =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
@@ -99,7 +101,7 @@ public class Keyword extends AppCompatActivity {
                 }
             };
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_various =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_dessert =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
@@ -118,7 +120,7 @@ public class Keyword extends AppCompatActivity {
                 }
             };
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_special =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_various =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
@@ -137,7 +139,7 @@ public class Keyword extends AppCompatActivity {
                 }
             };
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_size =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_special =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
@@ -151,12 +153,12 @@ public class Keyword extends AppCompatActivity {
                     int miniNumber = Float.toString(slider.getValues().get(0)).indexOf(".");
                     String minVal = Float.toString(slider.getValues().get(0)).substring(0, miniNumber);
                     int keyword_point= Integer.parseInt(minVal);
-                    keyword_point_arr[4]=keyword_point;
+                    keyword_point_arr[3]=keyword_point;
 //                    Log.d("DualThumbSeekbar ", "onStopTrackingTouch minPrice : " + minVal);
                 }
             };
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_Landscape =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_size =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
@@ -175,7 +177,7 @@ public class Keyword extends AppCompatActivity {
                 }
             };
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_concentration =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_Landscape =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
@@ -194,7 +196,7 @@ public class Keyword extends AppCompatActivity {
                 }
             };
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_trendy =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_concentration =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
@@ -208,31 +210,13 @@ public class Keyword extends AppCompatActivity {
                     int miniNumber = Float.toString(slider.getValues().get(0)).indexOf(".");
                     String minVal = Float.toString(slider.getValues().get(0)).substring(0, miniNumber);
                     int keyword_point= Integer.parseInt(minVal);
-                    keyword_point_arr[7]=keyword_point;
-//                    Log.d("DualThumbSeekbar ", "onStopTrackingTouch minPrice : " + minVal);
-                }
-            };
-
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_unique =
-            new RangeSlider.OnSliderTouchListener() {
-                @Override
-                public void onStartTrackingTouch(RangeSlider slider) {
-//                    바가 시작하면 동작하는 부분
-                }
-
-                @Override
-                public void onStopTrackingTouch(RangeSlider slider) {
-//                    유저가 바에서 손을 떄었을때 동작하는 함수
-//                    slider.getValues() 값이 [0.0, 5.0]처럼 배열로 값이 들어있다.
-                    int miniNumber = Float.toString(slider.getValues().get(0)).indexOf(".");
-                    String minVal = Float.toString(slider.getValues().get(0)).substring(0, miniNumber);
-                    int keyword_point= Integer.parseInt(minVal);
+                    keyword_point_arr[7]=4-keyword_point;
                     keyword_point_arr[8]=keyword_point;
 //                    Log.d("DualThumbSeekbar ", "onStopTrackingTouch minPrice : " + minVal);
                 }
             };
 
-    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_price =
+    private final RangeSlider.OnSliderTouchListener rangeSliderTouchListener_trendy =
             new RangeSlider.OnSliderTouchListener() {
                 @Override
                 public void onStartTrackingTouch(RangeSlider slider) {
